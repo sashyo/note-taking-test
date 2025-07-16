4cc74ffd2c4604427beb81f8ea7fa1423e4d1958
Sasha Le <iamsasha.le@gmail.com>
2025-07-16 12:11:55 +1000

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/note-taking-test/",
   server: {
     host: "::",
     port: 3000,
