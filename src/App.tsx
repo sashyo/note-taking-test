fad3f7d73109b7e351ad629f749811c6be8e7fcf
Sasha Le <iamsasha.le@gmail.com>
2025-07-16 12:24:50 +1000

    another fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { TideCloakProvider } from "@tidecloak/react";
 import adapter from "../tidecloakAdapter.json";
 import Index from "./pages/Index";
@@ -18,14 +18,14 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={base}>
+        <HashRouter basename={base}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth/redirect" element={<AuthRedirect />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </TideCloakProvider>
